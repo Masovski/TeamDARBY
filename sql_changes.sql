9cb83d1a13bca132a1b818f5da39a138ad5d2f8b
Stephan Kol <stephoto@abv.bg>
2014-08-24 22:39:09 +0300

    latest change of sql file

--- sql_changes.sql
+++ sql_changes.sql
@@ -23,15 +23,54 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура на таблица `posts`
+--
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `postID` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `post` text COLLATE utf8_unicode_ci NOT NULL,
+  `date_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `userID` int(4) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`postID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- Схема на данните от таблица `posts`
+--
+
+INSERT INTO `posts` (`postID`, `title`, `post`, `date_added`, `userID`, `active`) VALUES
+(1, 'Test Post', 'Test PostTest PostTest PostTest Post', '2014-08-24 10:33:12', 0, 1),
+(2, 'Test PostTest Post', 'Test PostTest PostTest PostTest \r\nPostTest PostTest PostTest PostTest PostTest Post\r\n123', '2014-08-24 10:35:03', 0, 1),
+(3, 'Test PostTest Post', 'Test PostTest PostTest PostTest PostTest PostTest PostTest PostTest PostTest Post', '2014-08-24 10:35:07', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `userID` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `mail` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `user_type` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Структура на таблица `usersinfo`
 --
 
 CREATE TABLE IF NOT EXISTS `usersinfo` (
   `userID` int(10) NOT NULL AUTO_INCREMENT,
-  `accName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `userFirstName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `userLastName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `userCity` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `user_first_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_last_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `city` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
   `birth` date DEFAULT NULL,
   `gender` char(2) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`userID`)
