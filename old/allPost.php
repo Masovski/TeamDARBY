240ecd675309421e9d420b763acadd8c63ceba0a
Martin <martin85@abv.bg>
2014-08-24 21:20:36 +0300

    Upload CI fw with some init data Move old commits to folder "old" Create "html template" folder

---
+++ old/allPost.php
@@ -0,0 +1,13 @@
+<?php
+session_start() ;
+
+include 'header.php' ;
+
+?>
+
+<div>Tuk 6te ima postove! </div>
+
+<?php
+include 'footer.php';
+
+?>
---
+++ old/header.php
@@ -0,0 +1,40 @@
+﻿<!DOCTYPE html>
+<html>
+    <head>
+        <title>Garby's BLOG</title>
+        <meta charset="UTF-8"> 
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="shortcut icon" type="image/x-icon" href="images/mara.png" />   
+    </head>
+ 	<body>
+	<header>
+	<img alt="SlothUni logo" class="logo" src="images/mara.png"/>
+	<h1><a href="index.php"> The real life whit the Blog of Team "Darby"</a></h1>
+	</header>
+	<nav>
+	<ul>
+		<li><a href="index.php">Начало</a></li>
+		<li><a href="For_us.php">За нас</a></li>
+		<li><a href="Articles.php">Статии</a></li>
+		<li><a href="Suggest_theme.php">Предложи тема на статия</a></li>
+	</ul>
+	</nav>
+ 	<aside>
+	<ul>
+ 		<li><a href="http://www.softuni.bg">Софт Уни</a></li>
+		<li><a href="#">Нещо</a></li>
+		<li><a href="#">Друго нещо</a></li>
+		<li><a href="#">Трето нещо</a></li>
+	</ul>
+ 	</aside>
+	<aside>	
+	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+		<input type="hidden" name="cmd" value="_s-xclick">
+		<input type="hidden" name="hosted_button_id" value="93YL6Q3653M3G">
+		<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
+		<img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+	</form>
+
+ 	</aside>
+	</body>
+	</html>
