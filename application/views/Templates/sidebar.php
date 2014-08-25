562f2d55141b92ba19b943ece613fd68564be3b9
MeLean <milen820430@gmail.com>
2014-08-25 22:53:05 +0300

    DONATE

--- application/views/Templates/sidebar.php
+++ application/views/Templates/sidebar.php
@@ -55,8 +55,13 @@
 
     <!-- Side Widget Well -->
     <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+        <h4>Support Us</h4>
+		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="93YL6Q3653M3G">
+			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
+			<img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+		</form>
     </div>
 
 </div>
