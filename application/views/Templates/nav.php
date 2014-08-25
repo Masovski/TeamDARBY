b6b0b281de72e6b0d32a041a850ab08867beaa50
Masovski <masovskitv@gmail.com>
2014-08-25 22:35:35 +0300

    Uploaded Bootstrap. This time for real.

---
+++ application/views/Templates/nav.php
@@ -0,0 +1,54 @@
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?= base_url(); ?>">Team Darby</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+<!-- Page Content -->
+<div class="container">
+    <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+            <h1 class="page-header">
+                Page Heading
+                <small>Secondary Text</small>
+            </h1>
+<?php if($this->session->userdata('userID')){ ?>
+    <p>
+        <?=$this->session->userdata('username')?>
+        <strong>
+            (<?=$this->session->userdata('user_type')?>)
+        </strong>
+        <a href="<?=base_url()?>users/logout">Logout</a>
+    </p>
+<?php } else { ?>
+    <a href="<?=base_url()?>users/login">Login</a>
+<?php } ?>
